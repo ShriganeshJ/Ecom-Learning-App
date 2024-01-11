b6b0dff1d920e737fb81160bbd6271b2af8de21e
Shriganesh Jadhav <shriganeshssj@gmail.com>
2024-01-11 19:43:46 +0530

    java 8 test ThreadUsingJava8

--- TestUtils/src/main/java/org/core/java/ThreadUsingJava8.java
+++ TestUtils/src/main/java/org/core/java/ThreadUsingJava8.java
@@ -1,15 +1,29 @@
 package org.core.java;
+
 /**
- *
- *
  * In that class we create thread using anyInner class
  * Using java 8 Lambda
  * Using inner class disadvantage is it create additional instance for innerClass
  */
 public class ThreadUsingJava8 extends Thread {
+    /**
+     *
+     * Using extend Thread class
+     */
+    @Override
+    public void run() {
+        System.out.println("Thread Using Extend " + Thread.currentThread().getId());
+
+    }
+
+    static {
+        ThreadUsingJava8 t = new ThreadUsingJava8();
+        t.start();
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Main Method Thread id"+Thread.currentThread().getId());
+        System.out.println("Main Method Thread id " + Thread.currentThread().getId());
         /**
          *
          * Using traditional approach-1
@@ -17,7 +31,7 @@ public class ThreadUsingJava8 extends Thread {
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("Using Inner class Thread id is"+Thread.currentThread().getId());
+                System.out.println("Using Inner class Thread id is " + Thread.currentThread().getId());
             }
         });
         t.start();
@@ -29,14 +43,11 @@ public class ThreadUsingJava8 extends Thread {
          *
          */
 
-        Thread t3 = new Thread(()-> System.out.println("Using Lambda Thread id is "+Thread.currentThread().getId()));
+        Thread t3 = new Thread(() -> System.out.println("Using Lambda Thread id is " + Thread.currentThread().getId()));
         t3.start();
 
 
     }
-
-
-
 
 
 }
