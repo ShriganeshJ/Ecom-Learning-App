2f45184f4edc460b9ed68b3c2e2fe68b87cf842e
Shriganesh Jadhav <shriganeshssj@gmail.com>
2024-01-11 09:19:26 +0530

    shared consumer

---
+++ JMS/src/main/java/org/ecoms/basic/FirstTopic.java
@@ -0,0 +1,56 @@
+package org.ecoms.basic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+//One To Many-Topic
+public class FirstTopic {
+    public static void main(String[] args) throws Exception {
+        InitialContext initialContext = null;
+
+        //Initial context create env
+        initialContext= new InitialContext();
+        //Get connection from factory
+        ConnectionFactory cf = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+        Connection connection=  cf.createConnection();
+        //Create Session
+        Session session = connection.createSession();
+        //Create Topic
+        Topic topic = (Topic) initialContext.lookup("topic/myTopic");
+        //create producer using topic
+        MessageProducer producer = session.createProducer(topic);
+
+        /************** Consumer ***************************/
+
+        //create Consumer1
+        MessageConsumer consumer1 = session.createConsumer(topic);
+        //create Consumer2
+        MessageConsumer consumer2 = session.createConsumer(topic);
+        //create Consumer3
+        MessageConsumer consumer3 = session.createConsumer(topic);
+
+        //Produce Message
+        TextMessage message = session.createTextMessage("I am topic Pub-sub");
+        producer.send(message);
+        System.out.println("Message Sent: " + message.getText());
+        //start connection
+        connection.start();
+
+
+
+
+        //Receive Message
+        TextMessage messageReceived1 = (TextMessage) consumer1.receive(5000);
+        System.out.println("Message Received consumer 1: " + messageReceived1.getText());
+
+        TextMessage messageReceived2 = (TextMessage) consumer2.receive(5000);
+        System.out.println("Message Received consumer 2: " + messageReceived2.getText());
+
+        TextMessage messageReceived3 = (TextMessage) consumer3.receive(5000);
+        System.out.println("Message Received consumer 3: " + messageReceived3.getText());
+
+        connection.close();
+        initialContext.close();
+
+
+    }
+}
---
+++ JMS/src/main/java/org/ecoms/pubsub/GoldRateAPI.java
@@ -0,0 +1,49 @@
+package org.ecoms.pubsub;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.ecoms.pubsub.pojo.GoldRate;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class GoldRateAPI {
+
+    public static void main(String[] args) throws NamingException {
+        InitialContext initialContext = new InitialContext();
+        Topic topic = (Topic) initialContext.lookup("topic/goldRateTopic");
+
+        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+             JMSContext jmsContext = cf.createContext()) {
+
+            GoldRate goldRate = new GoldRate();
+            goldRate.setLocalGold22KRates(52000.00);
+            goldRate.setLocalGold24Krates(58000.00);
+            goldRate.setGloablGold22Krates(45000.00);
+            goldRate.setGloabalGOld24Krates(48000.00);
+               jmsContext.createProducer().send(topic,goldRate);
+
+            System.out.println("Messge Send");
+
+            //for shared consumer
+            Thread.sleep(10000);
+            JMSContext jmsContext2 = cf.createContext();
+            GoldRate goldRate2 = new GoldRate();
+            goldRate2.setLocalGold24Krates(58000.00);
+
+            for(int i=0;i<10;i++)
+            {
+                jmsContext2.createProducer().send(topic,goldRate2);
+
+            }
+            System.out.println("Messge Send for shared consumers");
+
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    }
+
